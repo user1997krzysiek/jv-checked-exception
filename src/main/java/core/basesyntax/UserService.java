d1a49699307432a96b1c02ec43f7ac179a6be867
Krzysiek <krzysztof.orawski@wp.pl>
2026-01-10 19:56:54 +0100

    a

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,13 +6,9 @@ import java.sql.SQLOutput;
 
 public class UserService {
     private PasswordValidator validator = new PasswordValidator() {
-        public void registerUser(User user) {
-            try {
-                validator.validate(user.getPassword(), user.getRepeatPassword());
-                saveUser(user);
-            } catch (PasswordValidationException e) {
-                System.out.println("Wrongs Password");
-            }
+        public void registerUser(User user) throws PasswordValidationException {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         }
 
         public void saveUser(User user) {
