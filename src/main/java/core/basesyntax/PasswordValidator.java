edfc912feef7a0bf0c592dd7248620020154528b
Krzysiek <krzysztof.orawski@wp.pl>
2026-01-10 19:36:12 +0100

    userServic

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
-public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+import java.lang.reflect.UndeclaredThrowableException;
+import java.sql.SQLOutput;
+
+public class PasswordValidator extends Exception {
+    public void validate (String password, String repeatPassword) throws PasswordValidationException {
+        if (password != null && password.equals(repeatPassword) && (password.length() >= 10)) {
+            return;
+        } else {
+            throw new PasswordValidationException ("Wrong password");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,23 @@
 package core.basesyntax;
 
+import javax.crypto.spec.PSource;
+import java.io.IOException;
+import java.sql.SQLOutput;
+
 public class UserService {
-    public void registerUser(User user) {
-        //write your code here
+    private PasswordValidator validator = new PasswordValidator() {
+        public void registerUser(User user) {
+            try {
+                validator.validate(user.getPassword(), user.getRepeatPassword());
+                saveUser(user);
+            } catch (PasswordValidationException e) {
+                System.out.println("Wrongs Password");
+            }
+        }
+
+        public void saveUser(User user) {
+            System.out.println("User" + user.toString() + "Was saved  to detabase");
+        }
     }
 
     public void saveUser(User user) {
