81c6cb1ae9a694a65b252f5d5f2c87c70f3370d7
Krzysiek <krzysztof.orawski@wp.pl>
2026-01-11 16:57:27 +0100

    commit

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,14 +1,9 @@
 package core.basesyntax;
 
-import java.lang.reflect.UndeclaredThrowableException;
-import java.sql.SQLOutput;
-
-public class PasswordValidator extends Exception {
-    public void validate (String password, String repeatPassword) throws PasswordValidationException {
-        if (password != null && password.equals(repeatPassword) && (password.length() >= 10)) {
-            return;
-        } else {
-            throw new PasswordValidationException ("Wrong password");
+public class PasswordValidator {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        if (password == null || !password.equals(repeatPassword) || (password.length() < 10)) {
+            throw new PasswordValidationException("Wrong password");
         }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,17 +1,14 @@
 package core.basesyntax;
 
-import javax.crypto.spec.PSource;
-import java.io.IOException;
-import java.sql.SQLOutput;
-
 public class UserService {
-    private PasswordValidator validator = new PasswordValidator();
-        public void registerUser(User user) throws PasswordValidationException {
-            validator.validate(user.getPassword(), user.getRepeatPassword());
-            saveUser(user);
-        }
+    private final PasswordValidator validator = new PasswordValidator();
 
-        public void saveUser(User user) {
-            System.out.println("User" + user.toString() + "Was saved  to detabase");
-        }
+    public void registerUser(User user) throws PasswordValidationException {
+        validator.validate(user.getPassword(), user.getRepeatPassword());
+        saveUser(user);
     }
+
+    public void saveUser(User user) {
+        System.out.println("User" + user.toString() + "Was saved  to detabase");
+    }
+}
